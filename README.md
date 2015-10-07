1771b5c1024b0b71ccc7b6f3d919ec8808b69331
Pablo Meniño <pablo.menino@gmail.com>
2015-10-06 22:48:54 -0300

    Version 1.0

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-# MFW-ScreenShot
-Tool for ScreenShots
- 
+MFW ScreenShot Tool - A tool for take screenshots
+Version 1.0
+
+If you value your sanity ... beware ... http://mfw.com.ar ... is alive ...
+
+Copyright © 2009 - MFW TechNet - Pablo Meniño <pablo.menino@gmail.com>
+
+Supported Operating Systems:
+
+    * This script works in UNIX-like systems, tested on Fedora 12.
+
+Prerequisites:
+
+    * To use this script you need to have ImageMagick installed in your system.
+      Needed to take the screenshots.
+      ImageMagick - OpenMP http://www.imagemagick.org
---
+++ screenshot.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+##############################################################################
+#  ScreenShot Tool                                                           #
+#  Version 1.0                                                               #
+#                                                                            #
+#  Copyright © 2009 - Mouth For War - Pablo Meniño <pablo.menino@gmail.com>  #
+#                                                                            #
+#  -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- #
+#                                                                            #
+#  This program is free software; you can redistribute it and/or modify      #
+#  it under the terms of the GNU General Public License as published by      #
+#  the Free Software Foundation; either version 2 of the License, or         #
+#  (at your option) any later version.                                       #
+#                                                                            #
+#  This program is distributed in the hope that it will be useful,           #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+#  GNU General Public License for more details.                              #
+##############################################################################
+
+# The name of your file
+screenshot='screenshot';
+
+# Creates an unusual filename based on nanoseconds so that
+# you don't accidentally overwrite another screenshot.
+# The `backticks` tell the script to run another command (date).
+# Type 'man date' in the terminal for more info
+nano=`date '+%d-%m-%Y_%H-%M-%S'`;
+
+# Adds the file extension
+extension='.png';
+extensionTo='.jpg';
+
+# Generate a filename for this screenshot
+file="$HOME/Pictures/ScreenShots/$screenshot-$nano$extension";
+fileTo="$HOME/Pictures/ScreenShots/$screenshot-$nano$extensionTo";
+
+# Use ImageMagick to take the screenshot
+# Saves to ~/Pictures/ScreenShots
+import $file;
+convert  $file $fileTo;
